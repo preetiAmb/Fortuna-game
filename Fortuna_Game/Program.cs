5aff0c72d7dd0fc0b6890cf73a72ecc400824590
Preeti <35522305+preetiAmb@users.noreply.github.com>
2021-09-24 17:37:39 +0200

    Added functionality

--- Fortuna_Game/Program.cs
+++ Fortuna_Game/Program.cs
@@ -6,7 +6,110 @@ namespace Fortuna_Game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            {
+
+            int balance = 500;
+            int bet = 0;
+            string input;
+
+            Console.WriteLine("Welcome to the Play and pay game!");
+
+            Console.WriteLine("Your current balance is" + " " + balance + "pix");
+
+            while (true)
+            {
+                Console.WriteLine("How much pix do you want to bet? (You need to bet minimum of 50pix)");
+                input = Console.ReadLine();
+                int.TryParse(input, out bet);
+
+                if (bet < 50)
+                {
+                    Console.WriteLine("You choose a smaller amount!");
+                }
+                else if (bet > balance)
+                {
+                    Console.WriteLine("You dont have that much of amount!");
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            int lucky;
+
+            while (true)
+            {
+                Console.Write("Choose a lucky number between 1-6! ");
+                input = Console.ReadLine();
+                int.TryParse(input, out lucky);
+
+                if (lucky < 1)
+                {
+                    Console.WriteLine("You choose a smaller number! Please choose a number between 1-6");
+                }
+                else if (lucky > 6)
+                {
+                    Console.WriteLine("You choose a large number! Please choose a number between 1-6");
+                }
+                else
+                {
+                    break;
+                }
+            }
+                
+            balance -= bet;
+
+            Console.WriteLine("Throw a dice");
+
+            Random TorseDice = new Random();
+
+            int torse1 = TorseDice.Next(1, 7);
+            int torse2 = TorseDice.Next(1, 7);
+            int torse3 = TorseDice.Next(1, 7);
+
+            Console.WriteLine($"Dice 1 {torse1}");
+            Console.WriteLine($"Dice 2 {torse2}");
+            Console.WriteLine($"Dice 3 {torse3}");
+
+            int multi = 0;
+            if (torse1 == lucky && torse2 == lucky && torse3 == lucky)
+            {
+                multi = 4;
+            }
+            else if ((torse1 == lucky && torse2 == lucky) || (torse2 == lucky && torse3 == lucky))
+            {
+                multi = 3;
+            }
+            else if (torse1 == lucky || torse2 == lucky || torse3 == lucky)
+            {
+                multi = 2;
+            }
+
+            if (multi > 0)
+            {
+                Console.WriteLine("You worn " + (bet * multi));
+                balance += bet * multi;
+            }
+
+            Console.WriteLine("Your tolal balance is " + balance);
+
+           
+            if (balance < 50)
+            {
+                return;
+            }
+            Console.WriteLine("Will you play again?");
+            string play = Console.ReadLine();
+
+            if (play == "n")
+            {
+                Console.WriteLine("Thank you for playing!");            
+            }
+            else if (play == "y")
+            {
+                Console.Clear();
+            }
         }
     }
 }
